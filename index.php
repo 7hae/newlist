36307cc03ca8c14519f7461032db5500332f311c
honghuqing <shanhu66@126.com>
2014-07-08 16:17:58 +0800

    修改导航条

--- index.php
+++ index.php
@@ -1,45 +1,21 @@
 <?php // template used for the main page
 	get_header(); ?>
 <?php global $newsframe_options; $newsframe_settings = get_option( 'newsframe_options', $newsframe_options ); ?>
-<?php if ( is_active_sidebar( 'home-1' ) or is_active_sidebar( 'home-2' ) or is_active_sidebar( 'home-3') ) { ?>
 <div class="container">
-	<div class="col-lg-12">
-		<div class="four columns">
-		<?php do_action( 'before_widget' ); ?>
-				<?php if ( !dynamic_sidebar( 'home-1' ) ) : ?>
-				<?php endif; ?>
-		</div>
-		<div class="four columns">
-		<?php do_action( 'before_widget' ); ?>
-				<?php if ( !dynamic_sidebar( 'home-2' ) ) : ?>
-				<?php endif; ?>
-		</div>
-		<div class="four columns">
-		<?php do_action( 'before_widget' ); ?>
-				<?php if ( !dynamic_sidebar( 'home-3' ) ) : ?>
-				<?php endif; ?>
-		</div>
-	</div>
-</div>
-<?php } ?>
-
-<div class="container">
-<div class="col-lg-8" id="news_list">
+<div class="col-lg-8">
 	<?php if (have_posts()) : ?>
 		<?php while (have_posts()) : the_post(); ?>
-<div class="news list">
+	
+<div class="news">
 	<h2 class="index-title">
-		<span class="post_class"><?php the_category(' '); ?></span>
-		<span><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">
+		<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">
 			<?php the_title(); ?>
-		</a></span>
-		<i class="comment"><?php echo $post->comment_count;?></i>
+		</a>
 	</h2>
 	<p class="postinfo">
-		
-		<span class="spacer"></span>&nbsp;&nbsp;<i class="icon-user"></i> <?php the_author_posts_link(); ?>
-		<span class="spacer"></span>&nbsp;&nbsp; <?php the_time ('Y-m-d'); ?>
-		<span class="spacer"></span>&nbsp;&nbsp; 浏览：<i><?php echo $post->click;?></i>
+		<i class="icon-tags"></i>分类: <?php the_category(', '); ?>
+		<span class="spacer"></span><i class="icon-user"></i>&nbsp;&nbsp;作者: <?php the_author_posts_link(); ?>
+		<span class="spacer"></span><i class="icon-calendar"></i>&nbsp;&nbsp;发布时间: <?php the_time ('Y-m-d'); ?>
 	</p>
 	<?php $newsframevideopromote = get_post_meta ($post->ID, 'newsframe_video', true);
 	if (empty ($newsframevideopromote)) { ?>
@@ -55,35 +31,15 @@
 		<div class="col-lg-12">
 		<?php the_excerpt(); ?>
 		</div><!-- .latest-content -->
-		<div class="col-lg-12">
-			<p>
-			<span class="post_tags">标签:</span>&nbsp;&nbsp; 			
-			<?php
-				
-				$tags = wp_get_post_tags($post->ID);
-				foreach ($tags as $tag) {
-					?>
-					<span class="post_tag"><a href="/newslist/?tag=<?php echo $tag->name;?>"><?php echo $tag->name;?></a></span>&nbsp;&nbsp;
-					<?php
-				}
-					if(count($tags) == 0){
-						echo '<span class="post_tag_none">暂无标签</span>';
-					}
-			?>
-			</p>
-		</div>
-<div style="clear:both"></div>
+
 </div>
+<div style="clear:both"></div>
 	<?php endwhile; ?>
 	<?php endif; ?></div>
 <?php get_sidebar(); ?>
 </div>
+<section id="post-nav">
+	<?php posts_nav_link(' ', '<i class="icon-arrow-left"></i>', '<i class="icon-arrow-right"></i>'); ?>
+</section><!--End Navigation-->
 
-<div id="jiazai" class="container" style="display:none;">
-	<div class="col-md-12 news" style="text-align:center;">
-		<p class="neirong" style="padding:10px 30px">
-		<img src="<?php echo get_template_directory_uri();?>/images/zhuan.gif" style="display:inline" /><span id="neirong">正在加载，请稍候...</span>
-		</p>
-	</div>
-</div>
 <?php get_footer(); ?>
