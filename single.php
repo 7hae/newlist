ca78391047fccf1594c458b38ac6f1d6de57e7f1
honghuqing <shanhu66@126.com>
2014-07-14 17:31:42 +0800

    文章内容页
    Ujian,和相关新闻同事存在，手机显示相关新闻，电脑显示友荐的信息

--- single.php
+++ single.php
@@ -3,63 +3,155 @@
 	add_views($post->ID);
 ?>
 	<div class="container">
-		<div class="col-lg-8">
+		<div class="col-lg-8 news news_list">
 			<div class="single-article" role="main">
 			<?php if (have_posts()) : ?>
 			<?php while (have_posts()) : the_post(); ?>
 				<article <?php post_class(); ?>>
-				<div class="nav breadcrumbs">
-					<div itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php _e( 'Home', 'newsframe' ); ?></a> &#8250;
-						<?php the_category('<span class="category-space"> | </span>'); ?> &#8250; 
-						<span itemprop="title"><?php the_title(); ?></span>
+					<div class="nav breadcrumbs">
+						<div itemscope itemtype="http://data-vocabulary.org/Breadcrumb">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php _e( 'Home', 'newsframe' ); ?></a> &#8250;
+							<?php the_category('<span class="category-space"> | </span>'); ?> &#8250; 
+							<span itemprop="title"><?php the_title(); ?></span>
+						</div>
 					</div>
-				</div>
-				<h1 id="post-<?php the_ID(); ?>" class="article-title">
-				<?php the_title(); ?>
-				</h1>
-				<h2 class="article-subtitle">
-					<?php { $subtitle = get_post_meta
-					($post->ID, 'subtitle', $single = true);
-					if($subtitle !== '') echo $subtitle;} ?>
-				</h2><!-- #article-subtitle -->
+					<h2 id="post-<?php the_ID(); ?>" class="article-title">
+						<span class="post_class"><?php the_category(' '); ?></span>&nbsp;&nbsp;
+						<?php the_title(); ?>
+					</h2>
+					<h2 class="article-subtitle">
+						<?php { $subtitle = get_post_meta
+						($post->ID, 'subtitle', $single = true);
+						if($subtitle !== '') echo $subtitle;} ?>
+					</h2><!-- #article-subtitle -->
 				
-				<section class="byline">
-					所属分类: <?php the_category(', ') ?> &nbsp;&nbsp;
-					<span class="postinfo "><?php _e('<i class="icon-user"></i>' , 'newsframe' ); ?>
-					<?php the_author_posts_link(); ?></span> 
-					<span class="postinfo hideforprint"> &nbsp;&nbsp;<?php the_time('Y-m-d'); ?> 
-					</span>
-					<span>&nbsp;&nbsp;浏览量：<?php echo the_click($post->ID); ?></span>
-				</section><!-- .byline -->
-				<br />
-<!-- Featured Video -->
-<?php $newsframevideometa = get_post_meta ($post->ID, 'newsframe_video', true);
-	if ($newsframevideometa != '') {
-		?>
-			<div class="flex-video">
-				<?php echo get_post_meta($post->ID, 'newsframe_video', true); ?>
-			</div>
-	<?php } ?>
-	<section class="post-content">
-		<?php the_content(); ?>
-		<div style="clear:both;"></div>
-	<footer id="post-footer">
-		<span class="tag-links">
-		<?php the_tags(); ?>
-		</span>
-	<section class="relatedarticles">
-		<?php do_action( 'before_widget' ); ?>
-		<?php if ( !dynamic_sidebar( 'belowposts-sidebar' ) ) : ?>
-		<?php endif; ?>
-	</section>
-	</footer>
-	</section><!-- #post-content -->
-		</article>
-<?php endwhile; ?>
-	<section id="article-nav" style="margin-top:50px">
-		上一篇<?php previous_post_link(); ?> -- <?php next_post_link(); ?>下一篇
-	</section><!--End Article Navigation-->
+					<section class="byline">
+						<span class="postinfo "><?php _e('<i class="icon-user"></i>' , 'newsframe' ); ?>
+						<?php the_author_posts_link(); ?></span> 
+						<span class="postinfo hideforprint"> &nbsp;&nbsp;<?php the_time('Y-m-d'); ?> 
+						</span>
+						<span>&nbsp;&nbsp;浏览量：<?php echo the_click($post->ID); ?></span>
+					</section><!-- .byline -->
+					<br />
+					<!-- Featured Video -->
+					<?php $newsframevideometa = get_post_meta ($post->ID, 'newsframe_video', true);
+						if ($newsframevideometa != '') {
+							?>
+					<div class="flex-video">
+						<?php echo get_post_meta($post->ID, 'newsframe_video', true); ?>
+					</div>
+					<?php } ?>
+					<section class="post-content">
+						<img src="<?php ECHO get_post_thumbnail_url($post->ID);?>" />
+						<br />
+						<?php the_content(); ?>
+						<div style="clear:both;"></div>
+					
+					</section><!-- #post-content -->
+				</article>
+			<?php endwhile; ?>
+		<div class="col-lg-12" style="margin:30px 0">
+			<p>
+			<span class="post_tags">标签:</span>&nbsp;&nbsp; 			
+			<?php
+				$tags = wp_get_post_tags($post->ID);
+				foreach ($tags as $tag) {
+			?>
+					<span class="post_tag"><a href="/newslist/?tag=<?php echo $tag->name;?>"><?php echo $tag->name;?></a></span>&nbsp;&nbsp;
+			<?php
+				}
+				if(count($tags) == 0){
+					echo '<span class="post_tag_none">暂无标签</span>';
+				}
+			?>
+			</p>
+		</div>
+		<div class="col-lg-12" style="margin:30px 0">
+			<!-- JiaThis Button BEGIN -->
+				<div class="jiathis_style_32x32">
+					<a class="jiathis_button_qzone"></a>
+					<a class="jiathis_button_tsina"></a>
+					<a class="jiathis_button_tqq"></a>
+					<a class="jiathis_button_weixin"></a>
+					<a class="jiathis_button_renren"></a>
+					<a href="http://www.7hae.com/newslist" class="jiathis jiathis_txt jtico jtico_jiathis" target="_blank"></a>
+					<a class="jiathis_counter_style"></a>
+				</div>
+				<script type="text/javascript" src="http://v3.jiathis.com/code/jia.js" charset="utf-8"></script>
+			<!-- JiaThis Button END -->
+		</div>
+		<section id="article-nav" style="margin-top:50px">
+			上一篇<?php previous_post_link(); ?> -- <?php next_post_link(); ?>下一篇
+		</section><!--End Article Navigation-->
+
+
+<div class="col-lg-12 uj-news">
+<!-- UJian Button BEGIN -->
+<div class="ujian-hook"></div>
+<script type="text/javascript">var ujian_config = {num:4,itemTitle:'相关新闻：',picSize:152,textHeight:45,mouseoverColor:'#ffffff',borderColor:'#ffffff'};</script>
+<script type="text/javascript" src="http://v1.ujian.cc/code/ujian.js?uid=1948867"></script>
+<a href="http://www.ujian.cc" style="border:0;"><img src="http://img.ujian.cc/pixel.png" alt="友荐云推荐" style="border:0;padding:0;margin:0;" /></a>
+<!-- UJian Button END -->
+</div>
+
+<div class="col-lg-12 xg-news">
+	<h2>相关文章</h2>
+	<ul id="tags_related">
+	<?php
+		global $post;
+		$post_tags = wp_get_post_tags($post->ID);
+		if ($post_tags) {
+			foreach ($post_tags as $tag) {
+				// 获取标签列表
+				$tag_list[] .= $tag->term_id;
+			}
+
+			// 随机获取标签列表中的一个标签
+			$post_tag = $tag_list[ mt_rand(0, count($tag_list) - 1) ];
+
+			// 该方法使用 query_posts() 函数来调用相关文章，以下是参数列表
+			$args = array(
+				'tag__in' => array($post_tag),
+				'category__not_in' => array(NULL),  // 不包括的分类ID
+				'post__not_in' => array($post->ID),
+				'showposts' => 6,                           // 显示相关文章数量
+				'caller_get_posts' => 1
+			);
+			query_posts($args);
+
+			if (have_posts()) {
+				while (have_posts()) {
+					the_post(); update_post_caches($posts); ?>
+					<li class="col-lg-12">
+						<h2 class="index-title">
+							<span class="post_class"><?php the_category(' '); ?></span>&nbsp;&nbsp;&nbsp;
+							<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+						</h2>
+						<p class="postinfo">
+							<span class="spacer"></span>&nbsp;&nbsp;<i class="icon-user"></i> <?php the_author_posts_link(); ?>
+							<span class="spacer"></span>&nbsp;&nbsp; <?php the_time ('Y-m-d'); ?>
+							<span class="spacer"></span>&nbsp;&nbsp; 浏览：<i><?php echo $post->click;?></i>
+						</p>
+						<div class="col-lg-12">
+						<?php the_excerpt(); ?>
+						</div><!-- .latest-content -->
+					</li>
+				<?php
+				}
+			}
+			else {
+				echo '<li>* 暂无相关文章</li>';
+			}
+			wp_reset_query();
+		}
+		else {
+			echo '<li>* 暂无相关文章</li>';
+		}
+	?>
+	</ul>
+</div>
+
+<?php comments_template( '', true ); ?>
 <?php else : ?>
 <h2 class="center"><?php _e('Nothing is Here - Page Not Found', 'newsframe'); ?></h2>
 <div class="entry-content">
